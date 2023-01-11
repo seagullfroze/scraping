44a2826388a710ab4725538fa735ebbe60977665
seagullfroze <urijburleev2@gmail.com>
2023-01-11 18:01:41 +0200

    first1

---
+++ src/scraping/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Название населенного пункта')
+    slug = models.CharField(max_length=50, blank=True)
+    
+    
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
+
+
+    def __str__(self):
+        return self.name
