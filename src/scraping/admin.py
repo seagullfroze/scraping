63bb5b15e836917a6ff1a744fb30a96700ef8948
seagullfroze <urijburleev2@gmail.com>
2023-01-12 15:04:33 +0200

    first1

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import City
+from .models import City, Language, Vacancy
 
 admin.site.register(City)
+admin.site.register(Language)
+admin.site.register(Vacancy)
 # Register your models here.
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from .utils import from_cyrillic_to_eng
+
 
 class City(models.Model):
     name = models.CharField(max_length=50,
-                            verbose_name='Название населенного пункта')
-    slug = models.CharField(max_length=50, blank=True)
+                            verbose_name='Название населенного пункта',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
     
     
     class Meta:
@@ -14,3 +17,50 @@ class City(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Вид работы',
+                            unique=True)
+    slug = models.CharField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Работа'
+        verbose_name_plural = 'Вид работы'
+
+    def __str__(self):
+        return self.name
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+        super().save(*args, **kwargs)
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Загаловок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Работодатель')
+    description = models.TextField(verbose_name='Описание вакансии')
+    tel = models.CharField(max_length=20, verbose_name='Телефон')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Сфера деятельности')
+    timestamp = models.DateField(auto_now_add=True,)
+
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
+
+
+
+
